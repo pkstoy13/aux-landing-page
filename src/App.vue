f6e4e3c5b040176fbf82c9471e58e33601934f8e
Philip Stoyanov <pkstoyanov1@gmail.com>
2025-05-03 14:13:36 -0400

    set up vue app

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+//import { /*RouterLink,*/ RouterView } from 'vue-router'
+//import HelloWorld from './components/HelloWorld.vue'
+import TheNavbar from './components/TheNavbar.vue'
+
+</script>
+
+<template>
+  <header>
+    <!--<img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
+
+    <div class="wrapper">
+      <TheNavbar/>
+      <!--<HelloWorld msg="You did it!" /> -->
+
+      <!--
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    -->
+    </div>
+  </header>
+
+  <!--<RouterView />-->
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="navbar">
+    Aux
+  </div>
+</template>
+
+<style>
+  .navbar {
+    width: 100%;
+    background-color: #0388fc;
+    color: white;
+    padding: 8px;
+    top: 0;
+    left: 0;
+    position: fixed;
+
+  }
+</style>
