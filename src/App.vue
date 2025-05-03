5988318676426bb971a9b92baef3ac8eebb8de1d
Philip Stoyanov <pkstoyanov1@gmail.com>
2025-05-03 18:58:11 -0400

    styling work

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,12 @@
 //import HelloWorld from './components/HelloWorld.vue'
 import TheNavbar from './components/TheNavbar.vue'
 
+
 </script>
 
 <template>
   <header>
     <!--<img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
-
     <div class="wrapper">
       <TheNavbar/>
       <!--<HelloWorld msg="You did it!" /> -->
@@ -26,5 +26,43 @@ import TheNavbar from './components/TheNavbar.vue'
 </template>
 
 <style scoped>
+
+  @font-face {
+    font-family: 'Oxanium';
+    src: url('./assets/Oxanium-VariableFont_wght.ttf') format('truetype');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  * {
+    font-family: Oxanium;
+  }
+
+</style>
+
+<style>
+/* GLOBAL STYLES (not scoped) */
+body {
+  background-image: url('./assets/aux-background.svg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  animation: bgMove 5s ease-in-out infinite alternate;
+  margin: 0;
+  padding: 0;
+  overflow-x: hidden;
+}
+
+@keyframes bgMove {
+  0% {
+    background-position: 0% 0%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 100%;
+  }
+}
 
 </style>
